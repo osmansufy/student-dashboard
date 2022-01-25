c2440f7e26c577a5497df714f542db489e748c12
Osman Sufy <osmansufy20@gmail.com>
2022-01-25 11:55:09 +0600

    create coupon wc

--- includes/SaLearnersDbActivator.php
+++ includes/SaLearnersDbActivator.php
@@ -71,6 +71,7 @@ class SaLearnersDbActivator
         $isMultiple = $coupon['isMultiple'];
         $amount = $coupon['coupon_discount'];
         $discount_type = 'percent';
+        $admin_email[] = get_bloginfo('admin_email');
         $coupon_code = $coupon_title;
         $coupon_description = $coupon['coupon_description'];
         $coupon_minimum_amount = $coupon['coupon_minimum_amount'];
@@ -81,34 +82,60 @@ class SaLearnersDbActivator
         $coupon_individual_use = false;
         $coupon_exclude_sale_items = false;
         $coupon_expiry_date = '';
-        $coupon = array(
-            'post_title' => $coupon_code,
-            'post_content' => $coupon_description,
-            // 'post_excerpt' => $coupon_description,
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'post_type' => 'shop_coupon'
-        );
+        // $coupon = array(
+        //     'post_title' => $coupon_code,
+        //     'post_content' => $coupon_description,
+        //     // 'post_excerpt' => $coupon_description,
+        //     'post_status' => 'publish',
+        //     'post_author' => 1,
+        //     'post_type' => 'shop_coupon'
+        // );
 
 
         try {
-            $new_coupon_id = wp_insert_post($coupon);
-            update_post_meta($new_coupon_id, 'discount_type', $discount_type);
-            update_post_meta($new_coupon_id, 'coupon_amount', $amount);
-            update_post_meta($new_coupon_id, 'individual_use', $coupon_individual_use);
-            // update_post_meta($new_coupon_id, 'product_ids', $coupon_product_ids);
-            // update_post_meta($new_coupon_id, 'exclude_product_ids', $coupon_exclude_product_ids);
-            update_post_meta($new_coupon_id, 'usage_limit', '1');
-            update_post_meta($new_coupon_id, 'usage_limit_per_user', '1');
-            update_post_meta($new_coupon_id, 'limit_usage_to_x_items', '1');
-            update_post_meta($new_coupon_id, 'expiry_date', $coupon_expiry_date);
+            // Get an empty instance of the WC_Coupon Object
+            $coupon = new WC_Coupon();
 
-            // update_post_meta($new_coupon_id, 'minimum_amount', $coupon_minimum_amount);
-            update_post_meta($new_coupon_id, 'maximum_amount', $coupon_maximum_amount);
-            update_post_meta($new_coupon_id, 'exclude_sale_items', $coupon_exclude_sale_items);
-            update_post_meta($new_coupon_id, 'reward_type', $reward_type);
-            update_post_meta($new_coupon_id, 'reward_count', $reward_points_need);
-            update_post_meta($new_coupon_id, 'isMultiple', $isMultiple);
+            // Set the necessary coupon data (since WC 3+)
+            $coupon->set_code($coupon_code); // (string)
+            $coupon->set_description($coupon_description); // (string)
+            $coupon->set_discount_type($discount_type); // (string)
+            $coupon->set_amount($amount); // (float)
+            // $coupon->set_date_expires( $date_expires ); // (string|integer|null)
+            // $coupon->set_date_created( $date_created ); // (string|integer|null)
+            // $coupon->set_date_modified( $date_created ); // (string|integer|null)
+            // $coupon->set_usage_count( 1 ); // (integer)
+            $coupon->set_individual_use($coupon_individual_use); // (boolean) 
+            // $coupon->set_product_ids( $product_ids ); // (array)
+            // $coupon->set_excluded_product_ids( $excl_product_ids ); // (array)
+            $coupon->set_usage_limit(1); // (integer)
+            $coupon->set_usage_limit_per_user(1); // (integer)
+            $coupon->set_limit_usage_to_x_items(1); // (integer|null)
+
+            $coupon->set_minimum_amount($coupon_minimum_amount); // (float)
+            $coupon->set_maximum_amount($coupon_maximum_amount); // (float)
+            $coupon->set_email_restrictions($admin_email); // (array)
+
+            $coupon->save();
+            // update_post_meta($new_coupon_id, 'discount_type', $discount_type);
+            // update_post_meta($new_coupon_id, 'coupon_amount', $amount);
+            // update_post_meta($new_coupon_id, 'individual_use', $coupon_individual_use);
+            // // update_post_meta($new_coupon_id, 'product_ids', $coupon_product_ids);
+            // // update_post_meta($new_coupon_id, 'exclude_product_ids', $coupon_exclude_product_ids);
+            // update_post_meta($new_coupon_id, 'usage_limit', '1');
+            // update_post_meta($new_coupon_id, 'usage_limit_per_user', '1');
+            // update_post_meta($new_coupon_id, 'limit_usage_to_x_items', '1');
+            // update_post_meta($new_coupon_id, 'expiry_date', $coupon_expiry_date);
+            // // email restriction
+            // update_post_meta($new_coupon_id, 'email_restrictions', $admin_email);
+
+
+            // // update_post_meta($new_coupon_id, 'minimum_amount', $coupon_minimum_amount);
+            // update_post_meta($new_coupon_id, 'maximum_amount', $coupon_maximum_amount);
+            // update_post_meta($new_coupon_id, 'exclude_sale_items', $coupon_exclude_sale_items);
+            // update_post_meta($new_coupon_id, 'reward_type', $reward_type);
+            // update_post_meta($new_coupon_id, 'reward_count', $reward_points_need);
+            // update_post_meta($new_coupon_id, 'isMultiple', $isMultiple);
         } catch (Exception $e) {
         }
 
